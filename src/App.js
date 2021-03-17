777aadd4cd77cbd6c0073051075ec3eaaadb09a0
Simao Moreno <saiman13moreno@gmail.com>
2021-03-18 00:09:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Saiman's Website
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
